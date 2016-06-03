326790148bc59f5195ce031fbfd17c2125d95df4
vonway <haisafish@139.com>
2016-06-03 17:28:31 +0800

    Create tmux.v

---
+++ doc/tmux.v
@@ -0,0 +1,31 @@
+every thing about tmux
+
+prefix == C-b
+
+>> cmd line cmd
+= man tmux
+- tmux ls   show existed tmux list
+
+>> in tmux shortcut key
+= prefix ?  show all key map and functions
+common usage:
+  q     quit some mode
+  C-b c create a new window
+  C-b n switch to next window
+  C-b p switch to prev window
+  C-b 0-n switch to window which number is n
+  C-b & quit current window
+  C-b d temply detach
+  C-b " vsplit a panel
+  C-b % hsplit a panel
+  C-b o switch beteen panels
+  C-b ! close all panel
+  C-b x close current panel
+  C-b q show all panels number
+  number pressed after C-b q    swtich to n-th panel
+  C-b (arrow keys) switch to panel using arrow keys
+  
+  
+  
+  
+  
