6898361cb40358b9061dbf5c69c747d78e9b857e
vonway <vonwayalex@163.com>
2017-06-08 15:07:15 +0800

    Create install.sh

--- install.sh
+++ install.sh
@@ -17,6 +17,42 @@ fi
 
 cat ./bashrc >> ~/.bashrc
 
+echo "set tui border-kind space" > ~/.gdbinit
+
+cat >> ~/.screenrc << EOF
+altscreen on
+term xterm
+startup_message off
+autodetach on
+defscrollback 4096
+shell -$SHELL
+vbell off
+EOF
+
+cat >> ~/.gitconfig << EOF
+[alias]
+        st = status
+        ci = commit
+        co = checkout
+        br = branch
+        unstage = reset HEAD --
+        last = log -1 HEAD
+
+[user]
+        name = vonway
+        email = haisayu@gmail.com
+
+[core]
+        editor = vim
+        pager = less -r
+[diff]
+        tool = vimdiff
+[difftool]
+        prompt = false
+ 
+EOF
+
+
 echo "installing gitconfig"
 
 if [ -f "~/.gitconfig" ]; then
@@ -51,6 +87,7 @@ sudo make install
 
 echo "you may need to add /usr/local/lib to /etc/ld.so.conf"
 echo "  than exec ldconfig with root"
+echo "set your git user and email if you using git"
 
 
 
